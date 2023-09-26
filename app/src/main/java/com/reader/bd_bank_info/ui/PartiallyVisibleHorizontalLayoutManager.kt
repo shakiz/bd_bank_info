56e742a8a0c0e30234534064a88fbaf6a82c347c
shakiz <activities335@gmail.com>
2023-09-26 15:57:56 +0600

    Add partial visible layout manager

---
+++ app/src/main/java/com/reader/bd_bank_info/ui/PartiallyVisibleHorizontalLayoutManager.kt
@@ -0,0 +1,26 @@
+package com.reader.bd_bank_info.ui
+
+import android.content.Context
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class PartiallyVisibleHorizontalLayoutManager(
+    context: Context?,
+    private val minWidthPercentage: Float = 1f
+) : LinearLayoutManager(context, HORIZONTAL, false) {
+
+    var itemWidth = 0
+        private set
+
+    fun remainingOffset() = width - itemWidth
+
+    override fun checkLayoutParams(lp: RecyclerView.LayoutParams?): Boolean {
+        if (itemCount > 1 && minWidthPercentage != 1f && lp?.width != itemWidth) {
+            itemWidth = (width * minWidthPercentage).toInt()
+            lp?.width = itemWidth
+        } else if (minWidthPercentage == 1f) {
+            itemWidth = width
+        }
+        return super.checkLayoutParams(lp)
+    }
+}
