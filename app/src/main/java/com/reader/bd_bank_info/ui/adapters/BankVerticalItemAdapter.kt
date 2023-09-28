ae176930081202ef8cda58546614afb56d091891
shakiz <activities335@gmail.com>
2023-09-28 19:57:51 +0600

    Add bank list vertical layout and adapter

---
+++ app/src/main/java/com/reader/bd_bank_info/ui/adapters/BankVerticalItemAdapter.kt
@@ -0,0 +1,67 @@
+package com.reader.bd_bank_info.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.reader.bd_bank_info.R
+import com.reader.bd_bank_info.data.model.Bank
+import com.reader.bd_bank_info.databinding.RecyclerItemVerticalBankListBinding
+import com.reader.bd_bank_info.utils.*
+
+class BankVerticalItemAdapter : RecyclerView.Adapter<BankVerticalItemAdapter.BankVerticalViewHolder>() {
+    private val items = ArrayList<Bank>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BankVerticalViewHolder {
+        val binding = RecyclerItemVerticalBankListBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return BankVerticalViewHolder(binding).apply {
+            itemView.setOnClickListener {
+                //callback?.onNavRailItemClick(items[bindingAdapterPosition])
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: BankVerticalViewHolder, position: Int) {
+        holder.bindItem(items[position])
+    }
+
+    override fun getItemCount() = items.size
+
+    fun addItems(navRails: List<Bank>) {
+        val oldData = ArrayList(items)
+        items.clear()
+        items.addAll(navRails)
+
+        val diffUtilCallback = DiffUtilCallback(
+            oldData = oldData,
+            newData = items,
+            areItemsTheSame = { oldItem, newItem ->
+                return@DiffUtilCallback oldItem == newItem
+            },
+            areContentsTheSame = { oldItem, newItem ->
+                return@DiffUtilCallback oldItem == newItem
+            }
+        )
+        val diffResult = DiffUtil.calculateDiff(diffUtilCallback)
+
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    class BankVerticalViewHolder(val binding: RecyclerItemVerticalBankListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bindItem(bank: Bank) {
+            binding.tvTitle.text = bank.bankName
+            binding.tvType.text = bank.bankType
+            binding.tvEstablishedAtAndCategory.text = itemView.context.getString(R.string.x_s_established_x_s, bank.bankCategory, bank.establishedDate.toString())
+            bank.bankIconRes?.let {
+                binding.ivSlider.background = ContextCompat.getDrawable(itemView.context, it)
+            }
+        }
+    }
+}
