9256d90ef06d48e159bcde4c6776508a5a4a5c37
shakiz <activities335@gmail.com>
2023-09-28 12:04:12 +0600

    Update UI components of home and bank list

--- app/src/main/java/com/reader/bd_bank_info/ui/bank/BankListActivity.kt
+++ app/src/main/java/com/reader/bd_bank_info/ui/bank/BankListActivity.kt
@@ -2,20 +2,27 @@ package com.reader.bd_bank_info.ui.bank
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.reader.bd_bank_info.databinding.ActivityBankListBinding
 
 class BankListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBankListBinding
+    private lateinit var viewModel: BankViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBankListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        viewModel =
+            ViewModelProvider(this)[BankViewModel::class.java]
+
         initView()
         initListeners()
         initObservers()
+
+        viewModel.fetchBankList()
     }
 
     private fun initView() {
@@ -27,6 +34,10 @@ class BankListActivity : AppCompatActivity() {
     }
 
     private fun initObservers() {
+        viewModel.onBankListFetched().observe(this){bankList ->
+            bankList?.let {
 
+            }
+        }
     }
 }
