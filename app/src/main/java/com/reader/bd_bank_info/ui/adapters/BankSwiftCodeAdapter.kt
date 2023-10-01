dc8917b73c41bec30108ceb092f09ef8a7f26a06
shakiz <activities335@gmail.com>
2023-10-01 12:28:56 +0600

    Add swift code list activity and adapter for recycler

---
+++ app/src/main/java/com/reader/bd_bank_info/ui/adapters/BankSwiftCodeAdapter.kt
@@ -0,0 +1,64 @@
+package com.reader.bd_bank_info.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.reader.bd_bank_info.data.model.Bank
+import com.reader.bd_bank_info.databinding.RecyclerItemHorizontalBankListBinding
+import com.reader.bd_bank_info.databinding.RecyclerItemSwiftCodeBinding
+import com.reader.bd_bank_info.ui.bank.BankItemClickListener
+import com.reader.bd_bank_info.utils.*
+
+class BankSwiftCodeAdapter : RecyclerView.Adapter<BankSwiftCodeAdapter.BankSwiftCodeViewHolder>() {
+    private val items = ArrayList<Bank>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BankSwiftCodeViewHolder {
+        val binding = RecyclerItemSwiftCodeBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return BankSwiftCodeViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: BankSwiftCodeViewHolder, position: Int) {
+        holder.bindItem(items[position])
+    }
+
+    override fun getItemCount() = items.size
+
+    fun addItems(navRails: List<Bank>) {
+        val oldData = ArrayList(items)
+        items.clear()
+        items.addAll(navRails)
+
+        val diffUtilCallback = DiffUtilCallback(
+            oldData = oldData,
+            newData = items,
+            areItemsTheSame = { oldItem, newItem ->
+                return@DiffUtilCallback oldItem == newItem
+            },
+            areContentsTheSame = { oldItem, newItem ->
+                return@DiffUtilCallback oldItem == newItem
+            }
+        )
+        val diffResult = DiffUtil.calculateDiff(diffUtilCallback)
+
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    class BankSwiftCodeViewHolder(val binding: RecyclerItemSwiftCodeBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bindItem(bank: Bank) {
+            binding.tvBankTitle.text = bank.bankName
+            binding.tvBankSwiftCode.text = bank.swiftCode
+            binding.tvHotlineNumber.text = bank.hotlineNo.toString()
+            bank.bankIconRes?.let {
+                binding.ivSlider.background = ContextCompat.getDrawable(itemView.context, it)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/reader/bd_bank_info/ui/swiftcode/SwiftCodeListActivity.kt
@@ -0,0 +1,79 @@
+package com.reader.bd_bank_info.ui.swiftcode
+
+import android.content.Context
+import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.reader.bd_bank_info.databinding.ActivityBankListBinding
+import com.reader.bd_bank_info.ui.adapters.BankSwiftCodeAdapter
+import com.reader.bd_bank_info.ui.bank.BankViewModel
+
+class SwiftCodeListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBankListBinding
+    private lateinit var viewModel: BankViewModel
+    private val bankItemAdapter = BankSwiftCodeAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBankListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel =
+            ViewModelProvider(this)[BankViewModel::class.java]
+
+        initView()
+        initListeners()
+        initObservers()
+
+        viewModel.fetchBankList()
+    }
+
+    private fun initView() {
+        setSupportActionBar(binding.toolbar)
+        setupBankListView()
+    }
+
+    private fun initListeners() {
+        binding.toolbar.setNavigationOnClickListener { onBackPressed() }
+
+        binding.searchLayout.ibSearchButton.setOnClickListener {
+            closeKeyboard()
+            viewModel.searchBankItem(binding.searchLayout.etSearchName.text.toString())
+        }
+
+        binding.searchLayout.ibRefreshButton.setOnClickListener {
+            closeKeyboard()
+            binding.searchLayout.etSearchName.text?.clear()
+            viewModel.fetchBankList()
+        }
+    }
+
+    private fun initObservers() {
+        viewModel.onBankListFetched().observe(this) { bankList ->
+            bankList?.let {
+                bankItemAdapter.addItems(bankList)
+            }
+        }
+    }
+
+    private fun setupBankListView() {
+        binding.rvBankList.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.rvBankList.adapter = bankItemAdapter
+    }
+
+    private fun closeKeyboard() {
+        currentFocus?.let {
+            val manager: InputMethodManager = getSystemService(
+                Context.INPUT_METHOD_SERVICE
+            ) as InputMethodManager
+            manager
+                .hideSoftInputFromWindow(
+                    it.windowToken, 0
+                )
+        }
+    }
+}
