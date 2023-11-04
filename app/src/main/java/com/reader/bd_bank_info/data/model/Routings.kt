7113a8f9b673602893a8c820044ed302489f3aee
shakiz <activities335@gmail.com>
2023-11-05 00:30:21 +0600

    Update routing model with default values

--- app/src/main/java/com/reader/bd_bank_info/data/model/Routings.kt
+++ app/src/main/java/com/reader/bd_bank_info/data/model/Routings.kt
@@ -1,8 +1,8 @@
 package com.reader.bd_bank_info.data.model
 
 data class Routings(
-    val bankId: Int,
-    val areaName: String,
-    val branchName: String,
-    val routingNo: Int
+    val bankId: Int? = 0,
+    val areaName: String? = "",
+    val branchName: String? = "",
+    val routingNo: Int? = 0
 )
