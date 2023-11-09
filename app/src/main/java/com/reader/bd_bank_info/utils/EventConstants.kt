6a90fb251453461297e44b0264a7d4d03a77f05c
shakiz <activities335@gmail.com>
2023-11-09 20:49:58 +0600

    Add event names for analytics

---
+++ app/src/main/java/com/reader/bd_bank_info/utils/EventConstants.kt
@@ -0,0 +1,11 @@
+package com.reader.bd_bank_info.utils
+
+const val BANK_DETAILS_TAPPED = "bank_details_tapped"
+const val BANK_HOTLINE_NO_TAPPED = "bank_hotline_no_tapped"
+const val BANK_MAIL_TAPPED = "bank_mail_tapped"
+const val BANK_ROUTING_LIST_TAPPED = "bank_routing_list_tapped"
+const val BANK_ROUTING_TAPPED = "bank_routing_tapped"
+const val CURRENCY_TAPPED = "currency_tapped"
+const val STOCK_MARKET_TAPPED = "stock_market_tapped"
+const val SWIFT_CODE_TAPPED = "swift_code_tapped"
+const val SINGLE_BANK_NAME = "bank_name"
