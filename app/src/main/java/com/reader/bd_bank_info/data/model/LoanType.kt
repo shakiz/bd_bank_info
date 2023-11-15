6ad123c66f3bef4a2484ccfd74aa884b88cbbf86
shakiz <activities335@gmail.com>
2023-11-16 00:19:45 +0600

    Add loan type model

---
+++ app/src/main/java/com/reader/bd_bank_info/data/model/LoanType.kt
@@ -0,0 +1,7 @@
+package com.reader.bd_bank_info.data.model
+
+data class LoanType(
+    val loanTypeName: String? = null,
+    val loanTypeIcon: String? = null,
+    val navigationUrl: String? = null
+)
