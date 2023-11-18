d52f9c5d1d105e0b5cb2925872272e024758a832
shakiz <activities335@gmail.com>
2023-11-18 14:28:17 +0600

    Add popular loan model

---
+++ app/src/main/java/com/reader/bd_bank_info/data/model/loan/PopularLoan.kt
@@ -0,0 +1,8 @@
+package com.reader.bd_bank_info.data.model.loan
+
+data class PopularLoan(
+    val bankId: Int? = null,
+    val bankName: String? = null,
+    val loanName: String? = null,
+    val navigationUrl: String? = null
+)
