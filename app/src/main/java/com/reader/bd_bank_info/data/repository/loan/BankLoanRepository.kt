366feb1fa5925852a30481aa6026a58f3512e4aa
shakiz <activities335@gmail.com>
2023-11-18 14:32:01 +0600

    Add local data for popular loan

--- app/src/main/java/com/reader/bd_bank_info/data/repository/loan/BankLoanRepository.kt
+++ app/src/main/java/com/reader/bd_bank_info/data/repository/loan/BankLoanRepository.kt
@@ -1,8 +1,10 @@
 package com.reader.bd_bank_info.data.repository.loan
 
 import com.reader.bd_bank_info.data.model.loan.LoanType
+import com.reader.bd_bank_info.data.model.loan.PopularLoan
 
 
 interface BankLoanRepository {
     suspend fun fetchBankLoanTypeList() : List<LoanType>
+    suspend fun fetchPopularBankLoanList() : List<PopularLoan>
 }
--- app/src/main/java/com/reader/bd_bank_info/data/repository/loan/BankLoanRepositoryImpl.kt
+++ app/src/main/java/com/reader/bd_bank_info/data/repository/loan/BankLoanRepositoryImpl.kt
@@ -3,11 +3,16 @@ package com.reader.bd_bank_info.data.repository.loan
 import com.reader.bd_bank_info.R
 import com.reader.bd_bank_info.data.datasource.HomeApi
 import com.reader.bd_bank_info.data.model.loan.LoanType
+import com.reader.bd_bank_info.data.model.loan.PopularLoan
 
 class BankLoanRepositoryImpl(private val homeApi: HomeApi) : BankLoanRepository {
 
     override suspend fun fetchBankLoanTypeList(): List<LoanType> {
         return fetchBankLoanTypeLocalList()
+    }
+
+    override suspend fun fetchPopularBankLoanList(): List<PopularLoan> {
+        return fetchPopularBankLoanLocalList()
     }
 
     private fun fetchBankLoanTypeLocalList() : List<LoanType>{
@@ -20,6 +25,14 @@ class BankLoanRepositoryImpl(private val homeApi: HomeApi) : BankLoanRepository 
             LoanType(loanTypeIcon = R.drawable.ic_sme_loan, loanTypeName = "SME Loan", navigationUrl = "https://www.banksbd.org/loans/sme-loan.html"),
             LoanType(loanTypeIcon = R.drawable.ic_business_loan, loanTypeName = "Business Loan", navigationUrl = "https://www.banksbd.org/loans/business-loan.html"),
             LoanType(loanTypeIcon = R.drawable.ic_job_loan, loanTypeName = "Overseas Job Loan", navigationUrl = "https://www.banksbd.org/loans/overseas-job-loan.html")
+        )
+    }
+
+    private fun fetchPopularBankLoanLocalList(): List<PopularLoan>{
+        return listOf(
+            PopularLoan(bankId = 15, bankName = "DBBL", loanName = "DBBL Jokhon Tokhon Personal Loan", navigationUrl = "https://www.banksbd.org/dbbl/dbbl-jokhon-tokhon-personal-loan.html"),
+            PopularLoan(bankId = 31, bankName = "NCC Bank", loanName = "Education Loan by NCC Bank", navigationUrl = "https://www.banksbd.org/nccl/education-loan.html"),
+            PopularLoan(bankId = 31, bankName = "NCC Banl", loanName = "Personal Loan by NCC Bank", navigationUrl = "")
         )
     }
 }
