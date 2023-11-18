b36bba620e47052e63d8e418af185fc539784f41
shakiz <activities335@gmail.com>
2023-11-18 14:18:53 +0600

    Add loan type icons

--- app/src/main/java/com/reader/bd_bank_info/data/repository/loan/BankLoanRepositoryImpl.kt
+++ app/src/main/java/com/reader/bd_bank_info/data/repository/loan/BankLoanRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.reader.bd_bank_info.data.repository.loan
 
+import com.reader.bd_bank_info.R
 import com.reader.bd_bank_info.data.datasource.HomeApi
 import com.reader.bd_bank_info.data.model.LoanType
 
@@ -11,14 +12,14 @@ class BankLoanRepositoryImpl(private val homeApi: HomeApi) : BankLoanRepository 
 
     private fun fetchBankLoanTypeLocalList() : List<LoanType>{
         return listOf(
-            LoanType(loanTypeIcon = 0, loanTypeName = "Car Loan", navigationUrl = "https://www.banksbd.org/loans/car-loan.html"),
-            LoanType(loanTypeIcon = 0, loanTypeName = "Consumer Loan", navigationUrl = "https://www.banksbd.org/loans/consumer-loan.html"),
-            LoanType(loanTypeIcon = 0, loanTypeName = "Education Loan", navigationUrl = "https://www.banksbd.org/loans/education-loan.html"),
-            LoanType(loanTypeIcon = 0, loanTypeName = "Home Loan", navigationUrl = "https://www.banksbd.org/loans/home-loan.html"),
-            LoanType(loanTypeIcon = 0, loanTypeName = "Personal Loan", navigationUrl = "https://www.banksbd.org/loans/personal-loan.html"),
-            LoanType(loanTypeIcon = 0, loanTypeName = "SME Loan", navigationUrl = "https://www.banksbd.org/loans/sme-loan.html"),
-            LoanType(loanTypeIcon = 0, loanTypeName = "Business Loan", navigationUrl = "https://www.banksbd.org/loans/business-loan.html"),
-            LoanType(loanTypeIcon = 0, loanTypeName = "Overseas Job Loan", navigationUrl = "https://www.banksbd.org/loans/overseas-job-loan.html")
+            LoanType(loanTypeIcon = R.drawable.ic_car_loan, loanTypeName = "Car Loan", navigationUrl = "https://www.banksbd.org/loans/car-loan.html"),
+            LoanType(loanTypeIcon = R.drawable.ic_consumer_loan, loanTypeName = "Consumer Loan", navigationUrl = "https://www.banksbd.org/loans/consumer-loan.html"),
+            LoanType(loanTypeIcon = R.drawable.ic_education_loan, loanTypeName = "Education Loan", navigationUrl = "https://www.banksbd.org/loans/education-loan.html"),
+            LoanType(loanTypeIcon = R.drawable.ic_home_loan, loanTypeName = "Home Loan", navigationUrl = "https://www.banksbd.org/loans/home-loan.html"),
+            LoanType(loanTypeIcon = R.drawable.ic_personal_loan, loanTypeName = "Personal Loan", navigationUrl = "https://www.banksbd.org/loans/personal-loan.html"),
+            LoanType(loanTypeIcon = R.drawable.ic_sme_loan, loanTypeName = "SME Loan", navigationUrl = "https://www.banksbd.org/loans/sme-loan.html"),
+            LoanType(loanTypeIcon = R.drawable.ic_business_loan, loanTypeName = "Business Loan", navigationUrl = "https://www.banksbd.org/loans/business-loan.html"),
+            LoanType(loanTypeIcon = R.drawable.ic_job_loan, loanTypeName = "Overseas Job Loan", navigationUrl = "https://www.banksbd.org/loans/overseas-job-loan.html")
         )
     }
 }
