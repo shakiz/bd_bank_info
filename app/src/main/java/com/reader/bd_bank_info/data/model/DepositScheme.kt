3707cb463e5b5ee0ea4422ec7ced789317315a5d
shakiz <activities335@gmail.com>
2023-11-18 22:40:20 +0600

    Add deposit scheme model

---
+++ app/src/main/java/com/reader/bd_bank_info/data/model/DepositScheme.kt
@@ -0,0 +1,8 @@
+package com.reader.bd_bank_info.data.model
+
+data class DepositScheme(
+    val bankId: Int? = null,
+    val bankName: String? = null,
+    val depositName: String? = null,
+    val navigationUrl: String? = null
+)
