478b5e9d40f5395c9c1f61b37c397127758bc363
shakiz <activities335@gmail.com>
2023-11-19 21:28:27 +0600

    Add internet connectivity check for loan type click

--- app/src/main/java/com/reader/bd_bank_info/ui/loan/BankLoanTypeListActivity.kt
+++ app/src/main/java/com/reader/bd_bank_info/ui/loan/BankLoanTypeListActivity.kt
@@ -1,7 +1,12 @@
 package com.reader.bd_bank_info.ui.loan
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,10 +15,12 @@ import com.intuit.sdp.R
 import com.reader.bd_bank_info.AppInjector
 import com.reader.bd_bank_info.data.model.loan.LoanType
 import com.reader.bd_bank_info.databinding.ActivityBankLoanTypeListBinding
+import com.reader.bd_bank_info.network.InternetConnectivity
 import com.reader.bd_bank_info.ui.adapters.BankLoanTypeItemAdapter
 import com.reader.bd_bank_info.ui.commonwebview.CommonWebViewActivity
 import com.reader.bd_bank_info.utils.BANK_LOAN_TYPE_TAPPED
 import com.reader.bd_bank_info.utils.IDENTIFIER_BANK_LOANS
+import com.reader.bd_bank_info.utils.NO_INTERNET_DIALOG
 import com.reader.bd_bank_info.utils.SpaceItemDecoration
 import com.reader.bd_bank_info.utils.WEBVIEW_BUNDLE
 import com.reader.bd_bank_info.utils.dimenSize
@@ -66,15 +73,39 @@ class BankLoanTypeListActivity : AppCompatActivity(), BankLoanTypeItemAdapter.Ba
     }
 
     override fun onLoanTypeClick(loanType: LoanType) {
-        analytics.registerEvent(
-            IDENTIFIER_BANK_LOANS,
-            analytics.setData(
-                BANK_LOAN_TYPE_TAPPED,
-                loanType.loanTypeName?.lowercase()?.replace(" ", "_")
+        if(InternetConnectivity.checkConnectivity(this)){
+            analytics.registerEvent(
+                IDENTIFIER_BANK_LOANS,
+                analytics.setData(
+                    BANK_LOAN_TYPE_TAPPED,
+                    loanType.loanTypeName?.lowercase()?.replace(" ", "_")
+                )
             )
-        )
 
-        val bundle = CommonWebViewActivity.createIntent(this, loanType.navigationUrl, loanType.loanTypeName)
-        startActivity(Intent(this, CommonWebViewActivity::class.java).putExtra(WEBVIEW_BUNDLE, bundle))
+            val bundle = CommonWebViewActivity.createIntent(this, loanType.navigationUrl, loanType.loanTypeName)
+            startActivity(Intent(this, CommonWebViewActivity::class.java).putExtra(WEBVIEW_BUNDLE, bundle))
+        } else {
+            analytics.registerEvent(NO_INTERNET_DIALOG, analytics.setData(NO_INTERNET_DIALOG, BANK_LOAN_TYPE_TAPPED))
+            showNoInternetDialog()
+        }
+    }
+
+    private fun showNoInternetDialog(){
+        val dialog = Dialog(this, android.R.style.Theme_Dialog)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(com.reader.bd_bank_info.R.layout.dialog_no_internet)
+        dialog.window!!.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog.setCanceledOnTouchOutside(true)
+
+
+        dialog.setCanceledOnTouchOutside(true)
+        dialog.window!!.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+        dialog.window!!.setLayout(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+        dialog.show()
+
+        val tvTryAgain = dialog.findViewById<TextView>(com.reader.bd_bank_info.R.id.btnTryAgain)
+        tvTryAgain.setOnClickListener {
+            dialog.dismiss()
+        }
     }
 }
